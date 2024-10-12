80c99f514cb4d713e5e5643ff104be92c89a9a68
Amen Munir <2022cs703@student.uet.edu.pk>
2024-10-12 19:18:38 +0500

    WPF panels practice

---
+++ Panels practice/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<user> lst_backend = new ObservableCollection<user>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            lst_backend.Add(new user("haiqa", "abs"));
+            lst_backend.Add(new user("waleed", "abs"));
+            lst_backend.Add(new user("ibrahim", "abs"));
+            lst_frontend.ItemsSource = lst_backend;
+
+        }
+
+        private void btn_adduser_Click(object sender, RoutedEventArgs e)
+        {
+            lst_backend.Add(new user(Name = "amen", "abc"));
+
+        }
+
+        private void btn_deleteuser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lst_frontend.SelectedItem != null)
+            {
+                lst_backend.Remove(lst_frontend.SelectedItem as user);
+            }
+
+        }
+
+        private void btn_updateuser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lst_frontend.SelectedItem != null)
+            {
+                (lst_frontend.SelectedItem as user).Name = "absss";
+
+
+            }
+        }
+    }
+}
---
+++ Panels practice/Models/user.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;  
+
+internal class user : INotifyPropertyChanged
+{
+    private string name;
+    private string password;
+
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (name != value)
+            {
+                name = value;
+                
+                OnPropertyChanged("Name");
+            }
+        }
+    }
+
+    public string Password
+    {
+        get { return password; }
+        set
+        {
+            if (password != value)
+            {
+                password = value;
+               
+                OnPropertyChanged("Password");
+            }
+        }
+    }
+
+    public user(string name, string password)
+    {
+        Name = name;
+        Password = password;
+    }
+
+    
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
